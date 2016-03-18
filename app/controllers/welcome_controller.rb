0e340510171632a697f0ee91b297bbdef50956ef
cornmarx <cornelius.marx@dynport.de>
2016-03-18 17:30:44 +0100

    Some changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,21 @@
 class WelcomeController < ApplicationController
 	def index
-		
+		loc = params[:lang]
+  	redirect_to "/en" unless loc
+  	I18n.locale = loc
 	end
 
 	def about
-		
+
 	end
 
 	def contact
-	
+
 	end
 
 	def articles
 
 	end
-	
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'welcome#index'
-  get 'index' => 'welcome#index'
+  get '/:lang' => 'welcome#index'
 
   get 'about' => 'welcome#about'
 
   get 'contact' => 'welcome#contact'
 
   get 'articles' => 'welcome#articles'
-
-  get 'l/:lang' => 'lang#switch'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
